7aed2ef180f7d5140597106ce979f6709e8487cc
Denis <39928395+sed9xa@users.noreply.github.com>
2022-06-06 07:15:07 +0300

    End up width add and delete functions

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 let inputField = document.querySelector(".todolist__input");
 let taskArea = document.querySelector(".todolist__items");
-let todoItems = document.querySelectorAll(".todolist__item");
 
+let deleteIcon = document.querySelector('.iconify');
 let saveButton = document.querySelector(".todolist__save");
 let clearButton = document.querySelector(".todolist__clear");
 deleteTodo();
 function deleteTodo() {
   let deleteIcons = document.querySelectorAll(".iconify");
   for (let i = 0; i < deleteIcons.length; i++) {
-    console.log(deleteIcons[i].parentElement);
     deleteIcons[i].addEventListener("click", function (event) {
       deleteIcons[i].parentElement.remove();
       
@@ -19,12 +18,12 @@ function deleteTodo() {
 inputField.addEventListener("keypress", function (keyPressed) {
   if (keyPressed.which === 13 && inputField.value != "") {
     let listElement = document.createElement("div");
-    let deleteIcon = document.createElement("span");
-    deleteIcon.setAttribute("data-icon", "ant-design:delete-filled");
-    deleteIcon.className = "iconify";
+    let deleteIconClone = deleteIcon.cloneNode(true);
+    
+    deleteIconClone.className = "iconify";
     listElement.className = "todolist__item";
     listElement.innerHTML = "<p>" + inputField.value + "</p>";
-    listElement.appendChild(deleteIcon);
+    listElement.appendChild(deleteIconClone);
     taskArea.appendChild(listElement);
     deleteTodo();
   }
@@ -35,3 +34,9 @@ taskArea.addEventListener("click", function (event) {
     event.target.classList.toggle("checked");
   }
 });
+
+clearButton.addEventListener('click', function(){let todoItems = document.querySelectorAll(".todolist__item");
+  for (let item of todoItems){
+    item.remove();
+  }
+})
